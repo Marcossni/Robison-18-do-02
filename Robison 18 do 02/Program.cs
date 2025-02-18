bceb777fd067b4822ca7537f6dc3c3428136d1d6
049341 <049341@UNI-LAB2-44>
2025-02-18 19:52:16 -0300

    Adicionar arquivos de projeto.

---
+++ Robison 18 do 02/Program.cs
@@ -0,0 +1,24 @@
+﻿double A, B, C, R, S, O;
+
+Console.WriteLine("Escreva A:");
+A = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva B:");
+B = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva C:");
+C = int.Parse(Console.ReadLine());
+
+R = Math.Pow(A + B, 2);
+S = Math.Pow(B + C, 2);
+O = (R + S) / 2;
+Console.WriteLine("Resultado:");
+Console.WriteLine(O);
+
+if (O <= 100)
+  
+{
+    Console.WriteLine("Menor que 100");
+}
+else
+{
+    Console.WriteLine("Maior que 100");
+}
